9b5719c560c4ba4c4a3e3277e72a1a4b6a2ff9b3
Darren Chan <darrenusc@gmail.com>
2017-09-11 17:53:54 -0700

    Add delete function to tags

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,14 +9,6 @@ class Article < ApplicationRecord
     end.join(", ")
   end
 
-=begin  
-Alternative solution is to define new Tag#to_s method to override default
-
-def to_s
-  name
-end
-=end
-
   def tag_list=(tags_string)
     tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
     new_or_found_tags = tag_names.collect{|name| Tag.find_or_create_by(name: name)}
